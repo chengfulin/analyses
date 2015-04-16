177b2787baeb9bdf1a9195e767aacfda180f68fd
ChengFuLin <chengfulin0806@gmail.com>
2015-04-16 09:16:40 +0800

    Remove LastDef, Modify GEN to return set of Def

--- test/dfa.js
+++ test/dfa.js
@@ -3,142 +3,8 @@
  */
 var DFA = require('../lib/examples/dfa'),
     Set = require('../lib/set'),
-    cfgext = require('../lib/examples/cfgext');
-
-describe('DFA', function () {
-    it('should work for variable declaration only', function () {
-        var cfg = cfgext.getCFG('var x = 55, y = 10, tmp = 0;\n');
-        /// Get last def of 'x' at 'n1'
-        DFA.LastDEFs(cfg[2][1], 'x').values().should.eql([1]);
-        /// Get last def of 'y' at 'n1'
-        DFA.LastDEFs(cfg[2][1], 'y').values().should.eql([1]);
-        /// Get last def of 'tmp' at 'n1'
-        DFA.LastDEFs(cfg[2][1], 'tmp').values().should.eql([1]);
-    });
-
-    it('should work for redefinition', function () {
-        var cfg = cfgext.getCFG(
-            'var x = 55, y = 10, tmp = 0;\n' +
-            'x = 66;\n' +
-            'y = tmp = 1;'
-        );
-        /// Get last def of 'x' at 'n2'
-        DFA.LastDEFs(cfg[2][2], 'x').values().should.eql([2]);
-        /// Get last def of 'y' at 'n3'
-        DFA.LastDEFs(cfg[2][3], 'y').values().should.eql([3]);
-        /// Get last def of 'tmp' at 'n3'
-        DFA.LastDEFs(cfg[2][3], 'tmp').values().should.eql([3]);
-    });
-
-    it('should work for branches', function () {
-        var cfg = cfgext.getCFG(
-                'var x = 20, y = 5;\n' +
-                'if (x > y) {\n' +
-                    'var z = 10;\n' +
-                    'x = x % y;\n' +
-                '} else {\n' +
-                    'y = x;\n' +
-                '}'
-            );
-        /// Get last def of 'x' at node 'x > y'
-        DFA.LastDEFs(cfg[2][2], 'x').values().should.eql([1]);
-        /// Get last def of 'x' at node 'x = x % y'
-        DFA.LastDEFs(cfg[2][4], 'x').values().should.eql([4]);
-        /// Get last def of 'y' at node 'x = x % y'
-        DFA.LastDEFs(cfg[2][4], 'y').values().should.eql([1]);
-        /// Get last def of 'z' at node 'x = x % y'
-        DFA.LastDEFs(cfg[2][4], 'z').values().should.eql([3]);
-        /// Get last def of 'x' at node 'y = x'
-        DFA.LastDEFs(cfg[2][5], 'x').values().should.eql([1]);
-        /// Get last def of 'x' at node 'y = x'
-        DFA.LastDEFs(cfg[2][5], 'y').values().should.eql([5]);
-    });
-
-    it('should work with switch', function () {
-        var cfg = cfgext.getCFG(
-            'var test = 3, out;\n' +
-            'switch (test) {\n' +
-            'case 1:\n' +
-                'out = test;\n' +
-                'break;\n' +
-            'case 2:\n' +
-                'out = test * test;\n' +
-                'break;\n' +
-            'case 3:\n' +
-                'out = test * test * test;\n' +
-                'break;\n' +
-            'case 4:\n' +
-            'case 5:\n' +
-                'out = 0;\n' +
-                'break;\n' +
-            'default:\n' +
-                'out = -1;\n' +
-            '}\n' +
-            'var tmp = out;'
-        );
-        /// define 'out' in case '1'
-        cfg[2][2].astNode.test.value.should.eql(1);
-        DFA.LastDEFs(cfg[2][3], 'out').values().should.eql([3]);
-        DFA.LastDEFs(cfg[2][3], 'test').values().should.eql([1]);
-        /// get last def of 'out' at node 'var tmp = out'
-        DFA.LastDEFs(cfg[2][4], 'out').values().should.eql([3, 7, 9, 11, 14]);
-        DFA.LastDEFs(cfg[2][4], 'test').values().should.eql([1]);
-        /// define 'out' in case '2'
-        cfg[2][6].astNode.test.value.should.eql(2);
-        DFA.LastDEFs(cfg[2][7], 'out').values().should.eql([7]);
-        /// define 'out' in case '3'
-        cfg[2][8].astNode.test.value.should.eql(3);
-        DFA.LastDEFs(cfg[2][9], 'out').values().should.eql([9]);
-        /// define 'out' in case '4'
-        cfg[2][10].astNode.test.value.should.eql(4);
-        /// define 'out' in case '5'
-        cfg[2][12].astNode.test.value.should.eql(5);
-        DFA.LastDEFs(cfg[2][11], 'out').values().should.eql([11]);
-        /// define 'out' in 'default' case
-        DFA.LastDEFs(cfg[2][14], 'out').values().should.eql([14]);
-    });
-
-    it('should work for update expression', function () {
-        var cfg = cfgext.getCFG(
-            'var x = 5;\n' +
-            '++x;'
-        );
-        /// Get last def of 'x' at node '++x'
-        DFA.LastDEFs(cfg[2][2], 'x').values().should.eql([2]);
-    });
-
-    it('should work for object property assignment', function () {
-        var cfg = cfgext.getCFG(
-            'var obj = {};\n' +
-            'obj.prop = 123;'
-        );
-        /// Get last def of 'obj' at node 'obj.prop = 123'
-        DFA.LastDEFs(cfg[2][2], 'obj').values().should.eql([2]);
-    });
-
-    it('should work for loops', function () {
-        var cfg = cfgext.getCFG(
-            'var x = 5, y = 0;\n' +
-            'while(x > 0) {\n' +
-                'y += x;\n' +
-                '--x;\n' +
-                'var z = x;\n' +
-            '}'
-        );
-        /// Get the last def of 'x' at node 'while(x > 0)'
-        var lastDefN2 = DFA.LastDEFs(cfg[2][2], 'x').values();
-        lastDefN2.length.should.eql(2);
-        lastDefN2.should.containEql(1, 4);
-        /// Get the last def of 'x' at node 'y += x'
-        var lastDefN3 = DFA.LastDEFs(cfg[2][3], 'x').values();
-        lastDefN3.length.should.eql(2);
-        lastDefN3.should.containEql(1, 4);
-        /// Get the last def of 'y' at node 'while(x > 0)'
-        DFA.LastDEFs(cfg[2][3], 'y').values().should.eql([3]);
-        /// Get the last def of 'z' at node 'var z = x'
-        DFA.LastDEFs(cfg[2][5], 'z').values().should.eql([5]);
-    });
-});
+    cfgext = require('../lib/examples/cfgext'),
+    Def = require('../lib/examples/def');
 
 describe('KILL set', function () {
     it('should work for declaration only', function () {
@@ -267,11 +133,17 @@ describe('GEN set', function () {
             'var x = 55, y = 10, tmp = 0;\n'
         );
         /// GEN set of the entry node should be empty set
-        DFA.GEN(cfg[0].astNode).values().length.should.eql(0);
+        DFA.GEN(cfg[0]).values().length.should.eql(0);
         /// GEN set of the exit node should be empty set
-        DFA.GEN(cfg[1].astNode).values().length.should.eql(0);
-        /// GEN set of the VariableDeclaration node should be empty set
-        DFA.GEN(cfg[2][1].astNode).values().should.eql(['x', 'y', 'tmp']);
+        DFA.GEN(cfg[1]).values().length.should.eql(0);
+        /// declared variables should be in GEN
+        var n1GEN = DFA.GEN(cfg[2][1]).values();
+        n1GEN.length.should.eql(3);
+        n1GEN.should.containDeep([
+            new Def('x', 1),
+            new Def('y', 1),
+            new Def('tmp', 1)
+        ]);
     });
 
     it('should work for redefinition', function () {
@@ -280,18 +152,30 @@ describe('GEN set', function () {
             'x = 66;\n' +
             'y = tmp = 1;'
         );
-        /// GEN set of the AssignmentExpression node
-        DFA.GEN(cfg[2][2].astNode).values().should.eql(['x']);
-        /// GEN set of chain of assignment
-        DFA.GEN(cfg[2][3].astNode).values().should.eql(['y', 'tmp']);
+        /// redefinition should be in GEN
+        var n2GEN = DFA.GEN(cfg[2][2]).values();
+        n2GEN.length.should.eql(1);
+        n2GEN.should.containDeep([new Def('x', 2)]);
+        /// GEN set of chain of redefinitions
+        var n3GEN = DFA.GEN(cfg[2][3]).values();
+        n3GEN.length.should.eql(2);
+        n3GEN.should.containDeep([
+            new Def('y', 3),
+            new Def('tmp', 3)
+        ]);
     });
 
     it('should work for object declaration', function () {
         var cfg = cfgext.getCFG(
             'var x = 1, obj = {p: "prop"};'
         );
-        /// GEN set of the VariableDeclaration node with object initialization
-        DFA.GEN(cfg[2][1].astNode).values().should.eql(['x', 'obj']);
+        /// object name should be in GEN
+        var n1GEN = DFA.GEN(cfg[2][1]).values();
+        n1GEN.length.should.eql(2);
+        n1GEN.should.containDeep([
+            new Def('x', 1),
+            new Def('obj', 1)
+        ]);
     });
 
     it('should work for object property assignment', function () {
@@ -299,55 +183,73 @@ describe('GEN set', function () {
             'var obj = {};\n' +
             'obj.prop = 123;'
         );
-        /// GEN set of the AssignmentExpression of object MemberExpression
-        DFA.GEN(cfg[2][2].astNode).values().should.eql(['obj']);
+        /// change object's property means object changed
+        var n2GEN = DFA.GEN(cfg[2][2]).values();
+        n2GEN.length.should.eql(1);
+        n2GEN.should.containDeep([new Def('obj', 2)]);
     });
-
 
     it('should work for update expression', function () {
         var cfg = cfgext.getCFG(
             'var x = 5;\n' +
             'x++;'
         );
-        /// GEN set of the UpdateExpression node
-        DFA.GEN(cfg[2][2].astNode).values().should.eql(['x']);
+        /// update expression should be in GEN
+        var n2GEN = DFA.GEN(cfg[2][2]).values();
+        n2GEN.length.should.eql(1);
+        n2GEN.should.containDeep([new Def('x', 2)]);
     });
 
     it('should work for branches', function () {
         var cfg = cfgext.getCFG(
             'var x = 20, y = 5;\n' +
             'if (x > y) {\n' +
-            'var z = 10;' +
-            'x = x % y;\n' +
+                'var z = 10;' +
+                'x = x % y;\n' +
             '} else {\n' +
-            'y = x;\n' +
+                'y = x;\n' +
             '}'
         );
         /// GEN set of the node only binary expression should be empty
-        DFA.GEN(cfg[2][2].astNode).values().should.be.empty;
-        /// GEN set of the fist statement in the If statement
-        DFA.GEN(cfg[2][3].astNode).values().should.eql(['z']);
-        /// GEN set of the second statement in the If statement
-        DFA.GEN(cfg[2][4].astNode).values().should.eql(['x']);
-        /// GEN set of the node of the statement in the Else statement
-        DFA.GEN(cfg[2][5].astNode).values().should.eql(['y']);
+        DFA.GEN(cfg[2][2]).values().should.be.empty;
+        /// declaration in branch
+        var n3GEN = DFA.GEN(cfg[2][3]).values();
+        n3GEN.length.should.eql(1);
+        n3GEN.should.containDeep([new Def('z', 3)]);
+        /// assignment with binary expression
+        var n4GEN = DFA.GEN(cfg[2][4]).values();
+        n4GEN.length.should.eql(1);
+        n4GEN.should.containDeep([new Def('x', 4)]);
+        /// assignment in branch
+        var n5GEN = DFA.GEN(cfg[2][5]).values();
+        n5GEN.length.should.eql(1);
+        n5GEN.should.containDeep([new Def('y', 5)]);
     });
 
     it('should work for loops', function () {
         var cfg = cfgext.getCFG(
             'var x = 5, y = 0;\n' +
             'while(x > 0) {\n' +
-            'y += x;\n' +
-            '--x;\n' +
-            'var z = x;\n' +
+                'y += x;\n' +
+                '--x;\n' +
+                'var z = x;\n' +
             '}'
         );
-        /// GEN set of node for the test of the loop
-        DFA.GEN(cfg[2][2].astNode).values().should.be.empty;
-        /// GEN set of node inside loop
-        DFA.GEN(cfg[2][3].astNode).values().should.eql(['y']);
-        /// GEN set of the last node indes the loop
-        DFA.GEN(cfg[2][5].astNode).values().should.eql(['z']);
+        /// definition generated in loop
+        /// test condition won't be in GEN
+        DFA.GEN(cfg[2][2]).values().should.be.empty;
+        /// self assignment should also work
+        var n3GEN = DFA.GEN(cfg[2][3]).values();
+        n3GEN.length.should.eql(1);
+        n3GEN.should.containDeep([new Def('y', 3)]);
+        /// update expression should also work
+        var n4GEN = DFA.GEN(cfg[2][4]).values();
+        n4GEN.length.should.eql(1);
+        n4GEN.should.containDeep([new Def('x', 4)]);
+        /// declaration inside loop should also work
+        var n5GEN = DFA.GEN(cfg[2][5]).values();
+        n5GEN.length.should.eql(1);
+        n5GEN.should.containDeep([new Def('z', 5)]);
     });
 
     it('should work with switch', function () {
@@ -372,21 +274,32 @@ describe('GEN set', function () {
             '}\n' +
             'var tmp = out;'
         );
+        /// case 1
         cfg[2][2].astNode.test.value.should.eql(1);
-        DFA.GEN(cfg[2][3].astNode).values().should.eql(['out']);
+        var n3GEN = DFA.GEN(cfg[2][3]).values();
+        n3GEN.length.should.eql(1);
+        n3GEN.should.containDeep([new Def('out', 3)]);
         /// define 'out' in case '2'
         cfg[2][6].astNode.test.value.should.eql(2);
-        DFA.GEN(cfg[2][7].astNode).values().should.eql(['out']);
+        var n7GEN = DFA.GEN(cfg[2][7]).values();
+        n7GEN.length.should.eql(1);
+        n7GEN.should.containDeep([new Def('out', 7)]);
         /// define 'out' in case '3'
         cfg[2][8].astNode.test.value.should.eql(3);
-        DFA.GEN(cfg[2][9].astNode).values().should.eql(['out']);
+        var n9GEN = DFA.GEN(cfg[2][9]).values();
+        n9GEN.length.should.eql(1);
+        n9GEN.should.containDeep([new Def('out', 9)]);
         /// define 'out' in case '4'
         cfg[2][10].astNode.test.value.should.eql(4);
         /// define 'out' in case '5'
         cfg[2][12].astNode.test.value.should.eql(5);
-        DFA.GEN(cfg[2][11].astNode).values().should.eql(['out']);
+        var n11GEN = DFA.GEN(cfg[2][11]).values();
+        n11GEN.length.should.eql(1);
+        n11GEN.should.containDeep([new Def('out', 11)]);
         /// define 'out' in 'default' case
-        DFA.GEN(cfg[2][14].astNode).values().should.eql(['out']);
+        var n14GEN = DFA.GEN(cfg[2][14]).values();
+        n14GEN.length.should.eql(1);
+        n14GEN.should.containDeep([new Def('out', 14)]);
     });
 });
 
@@ -402,11 +315,11 @@ describe('USE set', function () {
         /// when initialized with another variable
         var n2USE = DFA.USE(cfg[2][2]).values();
         n2USE.length.should.eql(1);
-        n2USE.should.containEql('x');
+        n2USE.should.containDeep(['x']);
         /// when chained with other variables
         var n3USE = DFA.USE(cfg[2][3]).values();
         n3USE.length.should.eql(2);
-        n3USE.should.containEql('z', 'y');
+        n3USE.should.containDeep(['z', 'y']);
     });
 
     it('should work for redefinition', function () {
@@ -481,15 +394,15 @@ describe('USE set', function () {
         var n2USE = DFA.USE(cfg[2][2]).values();
         n2USE.length.should.eql(2);
         /// the object which owns called method should be in USE
-        n2USE.should.containEql('obj');
+        n2USE.should.containDeep(['obj']);
         /// passed argument should be in USE
-        n2USE.should.containEql('argu');
+        n2USE.should.containDeep(['argu']);
         var n3USE = DFA.USE(cfg[2][3]).values();
         n3USE.length.should.eql(2);
         /// the callee function should be in USE
-        n3USE.should.containEql('fun');
+        n3USE.should.containDeep(['fun']);
         /// passed argument should be in USE
-        n3USE.should.containEql('argu');
+        n3USE.should.containDeep(['argu']);
     });
 
     it('should work with switch', function () {
